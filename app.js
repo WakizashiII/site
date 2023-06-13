4c8df74fa43c2519893c25fe257ee0054f1d9f19
WakizashiII <116767598+WakizashiII@users.noreply.github.com>
2023-06-13 11:12:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ new Vue({
             description1: 'Gеnshin Impaсt 3.7',
             description2: 'Korepi "WITHOUT HWID"',
             description3: 'Updаte 31.05.2023',
-            image: 'https://cdn.discordapp.com/attachments/1118088905221210134/1118089509427499028/image.png',
+            image: 'https://cdn.discordapp.com/attachments/1118088905221210134/1118090031249227787/image.png',
             downloadLink: 'program/Korepi.rar'
 
           },
@@ -34,7 +34,7 @@ new Vue({
             description1: 'SOON...',
             description2: 'SOON...',
             description3: 'SOON...',
-            image: 'https://cdn.discordapp.com/attachments/1118088905221210134/1118089587198279730/image.png',
+            image: 'https://cdn.discordapp.com/attachments/1118088905221210134/1118090370564243496/image.png',
             downloadLink: 'SOON...'
            }
 		  
