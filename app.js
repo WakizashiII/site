dea2333bda4ea5e82a7a320ddb8b00ef0b1f65e3
WakizashiII <116767598+WakizashiII@users.noreply.github.com>
2023-06-23 14:49:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ new Vue({
             description2: 'Korepi "WITHOUT HWID"',
             description3: 'Updаte 31.05.2023',
             image: 'https://cdn.discordapp.com/attachments/1118088905221210134/1118091288894513162/image.png',
-            downloadLink: 'https://cdn.discordapp.com/attachments/1118088905221210134/1121766963547480124/crack.7z'
+            downloadLink: 'program/crack.7z'
 
           },
           {
